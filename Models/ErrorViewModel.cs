4a1ff57edca9d806e8d13636aad24bcb607f6d75
Ronja Norlén <rono2300@student.miun.se>
2025-03-04 14:09:10 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NetProjektNews.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
