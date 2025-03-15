73197403281c7e519335e8f8b9815f29430f431b
Ronja Norlén <rono2300@student.miun.se>
2025-03-15 10:44:48 +0100

    justerar HomeController för visning av nyheter i specifik kategori för medlemmar, hanterar visning av nyheter på nyhets-sida baserat på inloggad användare

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NetProjektNews.Models;
+using NetProjektNews.Data;
 
 namespace NetProjektNews.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context; // Databaskontext
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -18,9 +23,24 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    // Medlems-sida
+    public async Task<IActionResult> Privacy()
     {
-        return View();
+
+        // Kontrollera om context är null 
+        if (_context.Articles == null) 
+        {
+            return NotFound();
+        }
+
+        // Visa artiklar som ligger i kategori "Medlemmar"
+        var articles = await _context.Articles 
+        .Include(a => a.Category)
+        .Where(a => a.Category != null && a.Category.CategoryName == "Medlemmar")
+        .ToListAsync();
+
+
+        return View(articles);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
